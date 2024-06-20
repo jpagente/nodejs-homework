baca21b0548e8950d456253b59dadb38f250493b
Judielyn Pagente <git-commit-address>
2024-06-20 19:07:19 +0800

    Submitted working with email

--- validations/validation.js
+++ validations/validation.js
@@ -29,4 +29,15 @@
     subscription: Joi.string().valid("starter", "pro", "business"),
     });
 
-    export { contactValidation, favoriteValidation, signupValidation, subscriptionValidation };
+    // validation for email
+const emailValidation = Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required()
+      .messages({
+        "any.required": "Missing required email field",
+        "string.email": "Invalid email format",
+      }),
+  });
+
+    export { contactValidation, favoriteValidation, signupValidation, subscriptionValidation, emailValidation };
